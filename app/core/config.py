310d6ed248e98997dd002e468b1fe2377126bbb9
xiaosong-dada <1935571817@qq.com>
2026-01-25 01:05:45 +0800

    #配置项更新

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,10 @@ class Settings:
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT: int = int(os.getenv("POSTGRES_PORT"))
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
 
 settings = Settings()
