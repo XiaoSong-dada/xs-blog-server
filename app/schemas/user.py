a04681d66b26fdffb7bc2f41e3ec74f0e2c48d90
xiaosong-dada <1935571817@qq.com>
2026-01-25 01:06:16 +0800

    #用户类型

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,10 +2,20 @@ from app.schemas.base import Base
 from uuid import UUID
 from typing import Optional
 
-class User(Base):
+class UserInDB(Base):
     user_id: UUID
+    username: str
+    password:str
+    email: Optional[str] = None
+    status: str
+    is_admin: bool
+    avatar_url: Optional[str] = None
+    nick_name: Optional[str] = None
+    
+class UserOut(Base): 
     username: str
     email: Optional[str] = None
     status: str
     is_admin: bool
     avatar_url: Optional[str] = None
+    nick_name: Optional[str] = None
