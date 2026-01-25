b0e9026ae7409691642bee5be8d6598881b469fc
xiaosong-dada <1935571817@qq.com>
2026-01-25 23:41:17 +0800

    #删除用户

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,7 @@
 from app.db.transaction import transaction
-from app.repositories.user_repo import create_user
+from app.repositories.user_repo import create_user, delete_by_username
 from app.schemas.user import UserCreate, UserInDB
-from app.utils.password import get_password_hash
+from app.security.password import get_password_hash
 
 class UsernameTakenError(RuntimeError):
     """业务异常：用户名已存在"""
@@ -22,4 +22,16 @@ def register_user(user: UserCreate) -> None:
         ok = create_user(conn, user_in_db)
         if not ok:
             # 这属于业务失败，可控
-            raise UsernameTakenError("username already exists")
+            raise UsernameTakenError("用户已存在！")
+        
+def delete_user(username: str, admin_user) -> None:
+    if not admin_user["is_admin"]:
+        raise UsernameTakenError("无权限删除用户！")
+
+    if admin_user["username"] == username:
+        raise UsernameTakenError("不能删除自己！")
+
+    with transaction() as conn:
+        ok = delete_by_username(conn, username)
+        if not ok:
+            raise UsernameTakenError("用户不存在！")
